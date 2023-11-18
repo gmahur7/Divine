c1a86b2672d4da3346c42cdef62927ec32bae2fc
gmahur7 <gm7017308@gmail.com>
2023-11-18 11:15:47 +0530

    Changes

--- React/Components/Home.js
+++ React/Components/Home.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function Home(){
     return(
-        <div id='home'>
+        <div>
+        <div className="background-img"></div>
+      <div className="overlay"></div>
+        <div className="signup-container" id='home'>
             <h1>Home</h1>
-            <div>
-                <Link to='/adminlogin'>Admin Login</Link>
-                <Link to='/signup'>User SignUp</Link>
+            <div className='link-wrap'>
+                <Link to='/adminlogin' className='admin-link'>Admin Login</Link>
+                <Link to='/signup' className='admin-link'>User SignUp</Link>
             </div>
+        </div>
         </div>
     )
 }
--- React/Components/SignupForm.js
+++ React/Components/SignupForm.js
@@ -3,154 +3,167 @@ import axios from 'axios'
 import { Navigate, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
-    const navigate=useNavigate()
-    let date=new Date()
-    date=`${date.getFullYear()}${date.getMonth()+1}${date.getDate()}`
-    const [name, setName] = useState('')
-    const [fname, setFname] = useState('')
-    const [dob, setDob] = useState('')
-    const [email, setEmail] = useState('')
-    const [mob, setMob] = useState('')
-    const [add, setAdd] = useState('')
-    const [schoolName, setSchoolName] = useState('')
-    const [employeeCompany, setEmployeeCompany] = useState('')
-    const [profession, setprofession] = useState('')
-    const [course, setCourse] = useState('')
-    const [error, setError] = useState(false)
-    const [emailErr, setEmailErr] = useState(false)
-    const [mobErr, setMobErr] = useState(false)
-    const [msg, setMsg] = useState(false)
+  const navigate = useNavigate()
+  let date = new Date()
+  date = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`
+  const [name, setName] = useState('')
+  const [fname, setFname] = useState('')
+  const [dob, setDob] = useState('')
+  const [email, setEmail] = useState('')
+  const [mob, setMob] = useState('')
+  const [add, setAdd] = useState('')
+  const [schoolName, setSchoolName] = useState('')
+  const [employeeCompany, setEmployeeCompany] = useState('')
+  const [profession, setprofession] = useState('')
+  const [course, setCourse] = useState('')
+  const [error, setError] = useState(false)
+  const [emailErr, setEmailErr] = useState(false)
+  const [mobErr, setMobErr] = useState(false)
+  const [msg, setMsg] = useState(false)
 
-    async function submit() {
-        setMob(mob.toString())
-        if (!name || !email || !fname || !dob || !mob || !add||!course||!profession||!schoolName||!employeeCompany) setError(true)
+  async function submit() {
+    setMob(mob.toString())
+    if (!name || !email || !fname || !dob || !mob || !add || !course || !profession || !schoolName || !employeeCompany) setError(true)
+    else {
+      if (!email.includes('@') || email.slice(email.length - 4, email.length) !== '.com') {
+        setEmailErr(true)
+      } else {
+        if (mob.length == 10) {
+          let result = await fetch('http://localhost/signup', {
+            method: 'post',
+            body: JSON.stringify({ Created: date, Name: name, Email: email, FatherName: fname, DOB: dob, PhoneNo: mob, Address: add, Course: course, Profession: profession, SchoolName: schoolName, EmployeeCompany: employeeCompany, }),
+            headers: { 'Content-Type': 'application/json' }
+          })
+          result = await result.json()
+          if (result) {
+            setTimeout(() => {
+              navigate('/')
+              setMsg(false)
+            }, 4000)
+            setMsg(true)
+          }
+        }
         else {
-            if (!email.includes('@') || email.slice(email.length - 4, email.length) !== '.com') {
-                setEmailErr(true)
-            } else {
-                if(mob.length==10){
-                let result = await fetch('http://localhost/signup', {
-                    method: 'post',
-                    body: JSON.stringify({ Created:date,Name:name, Email:email, FatherName:fname, DOB:dob, PhoneNo:mob,Address:add,Course:course,Profession:profession,SchoolName:schoolName,EmployeeCompany:employeeCompany,}),
-                    headers: { 'Content-Type': 'application/json' }
-                })
-                result = await result.json()
-                if(result) {
-                    setTimeout(()=>
-                    {
-                        navigate('/')
-                        setMsg(false)
-                    },4000)
-                    setMsg(true)
-                }
-            }
-            else{
-                setMobErr(true)
-            }
+          setMobErr(true)
         }
-        }
+      }
     }
-  
-    function mobHandler(e){
-    setMob(e.target.value)
-    if(mobErr===true) setMobErr(false)
-    }
+  }
 
-    function reset(){
-        setAdd('')
-        setName('')
-        setFname('')
-        setEmail('')
-        setMob('')
-        setDob('')
-        setCourse('')
-        setEmployeeCompany('')
-        setSchoolName('')
-        setprofession('')
+  function mobHandler(e) {
+    setMob(e.target.value)
+    if (mobErr === true) setMobErr(false)
+  }
+
+  function reset() {
+    document.getElementById('courses').value=""
+    document.getElementById('profession').value=""
+    setAdd('')
+    setName('')
+    setFname('')
+    setEmail('')
+    setMob('')
+    setDob('')
+    setCourse('')
+    setEmployeeCompany('')
+    setSchoolName('')
+    setprofession('')
+    setError(false)
+    setMobErr(false)
+    setEmailErr(false)
+  }
+  useEffect(() => {
+    if (profession === 'Student') {
+      setEmployeeCompany('-')
+      setSchoolName('')
     }
-    useEffect(()=>{
-        if(profession==='Student'){
-            setEmployeeCompany('-')
-            setSchoolName('')
-        }
-        else if(profession==='Employee'){
-            setSchoolName('-')
-            setEmployeeCompany('')
-        }
-    },[profession])
-    return (
-        <div id='signup'>
-            <div>
-                <button onClick={()=>navigate('/')}>{'<-'}</button>
-            </div>
-            <h1>Enter Your Details : </h1>
-            <div>
-                <span>Enter Name </span><input value={name} onChange={(e) => setName(e.target.value)} type='text' />
-                <div>{error && !name && <p>Please Enter Name</p>}</div>
-            </div>
-            <div>
-                <span>Enter Father's Name </span><input value={fname} onChange={(e) => setFname(e.target.value)} type='text' />
-                <div>{error && !fname && <p>Please Father's Name</p>}</div>
-            </div>
-            <div>
-                <span>Enter D.O.B </span><input value={dob} onChange={(e) => setDob(e.target.value)} type='date' />
-                <div>{error && !dob && <p>Please Enter Date Of Birth </p>}</div>
-            </div>
-            <div>
-                <span>Enter Email </span><input value={email} onChange={(e) => setEmail(e.target.value)} type='email' />
-                <div>{error && !email && <p>Please Enter Email</p>}</div>
-                <div>{emailErr && <p>Please Enter Valid Email</p>}</div>
-            </div>
-            <div>
-                <span>Enter Contact No. </span><input value={mob} onChange={mobHandler} type='number' />
-                <div>{error && !mob && <p>Please Enter Contact No.</p>}</div>
-                <div>{mobErr && <p>Enter 10 Digit Contact No.</p>}</div>
-            </div>
-            <div>
-                <span>Enter Address </span><input value={add} onChange={(e) => setAdd(e.target.value)} type='text' />
-                <div>{error && !add && <p>Please Enter  Address</p>}</div>
-            </div>
-            <div>
-                <span>Select Course</span>
-                <select onChange={(e) => setCourse(e.target.value)}>
-                    <option value={''}>Courses</option>
-                    <option value={'Fullstack Web Developement'}>Fullstack Web Developement</option>
-                    <option value={'Frontend Web Developement'}>Frontend Web Developement</option>
-                    <option value={'Backend Web Developement'}>Backend Web Developement</option>
-                    <option value={'Cloud Engineering'}>Cloud Engineering</option>
-                </select>
-            </div>
-            <div>
-                <span>Are You a </span>
-                <select onChange={(e) => setprofession(e.target.value)}>
-                    <option value=''>select</option>
-                    <option value="Student">Student</option>
-                    <option value="Employee">Employee</option>
-                </select>
-                {profession === 'Student' ?
-                    <>
-                        <span>Enter School/College Name </span>
-                        <input type='text' value={schoolName} onChange={(e) => setSchoolName(e.target.value)} />
-                       {error&&!schoolName&&<p>Please Provide School/College Name </p>}
-                    </>
-                    : profession === 'Employee' ?
-                        <>
-                        <span>Enter Working Company Name </span>
-                        <input type='text' value={employeeCompany} onChange={(e) => setEmployeeCompany(e.target.value)} />
-                        {error&&!employeeCompany&&<p>Please Provide Employee Company </p>}
-                    </>
-                    : null
-                }
-            </div>
-            <div>
-                <button onClick={submit}>Submit</button>
-                <button onClick={reset}>Reset</button>
-            </div>
-            <div>
-                {msg&&<h2>You Are Successfully Signup</h2>}
-            </div>
+    else if (profession === 'Employee') {
+      setSchoolName('-')
+      setEmployeeCompany('')
+    }
+  }, [profession])
+  return (
+    <div className='main-wrapper'>
+      <div className="background-img"></div>
+      <div className="overlay"></div>
+      <div className="signup-container" id='signup'>
+        <div className="signup-header">
+          <button className='back-button' onClick={() => navigate('/')}>{'back'}</button>
+          <h1 className='signup-heading'>Sign up</h1>
         </div>
-    )
+        <div className='form-group'>
+          <input placeholder=" " className='form-control' value={name} onChange={(e) => setName(e.target.value)} type='text' />
+          <label>Enter Name</label>
+          {error && !name && <p className='error'>Please Enter Name</p>}
+        </div>
+        <div className='form-group'>
+          <input placeholder=" " className='form-control' value={fname} onChange={(e) => setFname(e.target.value)} type='text' />
+          <label>Enter Father's Name</label>
+          {error && !fname && <p className='error'>Please Father's Name</p>}
+        </div>
+        <div className='form-group'>
+          <input placeholder=' ' className='form-control' value={dob} onChange={(e) => setDob(e.target.value)} type='date' />
+          {error && !dob && <p className='error'>Please Enter Date Of Birth </p>}
+        </div>
+        <div className='form-group'>
+          <input placeholder=" " className='form-control' value={email} onChange={(e) => setEmail(e.target.value)} type='email' />
+          <label>Enter Email</label>
+          {error && !email && <p className='error'>Please Enter Email</p>}
+          {emailErr && <p className='error'>Please Enter Valid Email</p>}
+        </div>
+        <div className='form-group'>
+          <input placeholder=" " className='form-control' value={mob} onChange={mobHandler} type='number' />
+          <label>Enter Contact No.</label>
+          {error && !mob && <p className='error'>Please Enter Contact No.</p>}
+          {mobErr && <p className='error'>Enter 10 Digit Contact No.</p>}
+        </div>
+        <div className='form-group'>
+          <input placeholder=" " className='form-control' value={add} onChange={(e) => setAdd(e.target.value)} type='text' />
+          <label>Enter Address</label>
+          {error && !add && <p className='error'>Please Enter  Address</p>}
+        </div>
+        <div className='form-group'>
+          <select id='courses' className='form-control' onChange={(e) => setCourse(e.target.value)}>
+            <option value={''}>Courses</option>
+            <option value={'Fullstack Web Developement'}>Fullstack Web Developement</option>
+            <option value={'Frontend Web Developement'}>Frontend Web Developement</option>
+            <option value={'Backend Web Developement'}>Backend Web Developement</option>
+            <option value={'Cloud Engineering'}>Cloud Engineering</option>
+          </select>
+          {error && !course && <p className='error'>Please Select Course</p>}
+        </div>
+        <div>
+          <select id='profession' className='form-control' onChange={(e) => setprofession(e.target.value)}>
+            <option value=''>Are You a</option>
+            <option value="Student">Student</option>
+            <option value="Employee">Employee</option>
+          </select>
+          {error && !profession && <p className='error'>Please Select Profession</p>}
+          {profession === 'Student' ?
+            <div className="form-group">
+              <input placeholder=" " className="form-control" type='text' value={schoolName} onChange={(e) => setSchoolName(e.target.value)} />
+              <label>Enter School/College Name</label>
+              {error && !schoolName && <p className='error'>Please Provide School/College Name </p>}
+            </div>
+            : profession === 'Employee' ?
+              <div className="form-group">
+                <input placeholder=" " className="form-control" type='text' value={employeeCompany} onChange={(e) => setEmployeeCompany(e.target.value)} />
+                <label>Enter Working Company Name </label>
+                {error && !employeeCompany && <p className='error'>Please Provide Employee Company </p>}
+              </div>
+              : null
+          }
+        </div>
+        <div className='signup-button'>
+          <button onClick={submit}>Submit</button>
+          <button className="reset-btn" onClick={reset}>Reset</button>
+        </div>
+        <div>
+          {msg && <h2>You Are Successfully Signup</h2>}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Signup
